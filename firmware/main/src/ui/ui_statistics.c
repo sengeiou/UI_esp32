e79bae33b987d9664f4f9f42771bf0c9833f2bf0
Giuseppe Lafortezza <giuseppe.lafortezza@lavazza.com>
2021-10-21 10:21:04 +0200

    Added scrollable statistics

--- firmware/main/src/ui/ui_statistics.c
+++ firmware/main/src/ui/ui_statistics.c
@@ -11,6 +11,7 @@ ui_func_desc_t ui_statistics_func = {
 
 // /* LVGL objects defination */
 static lv_obj_t* obj_fullstats = NULL;
+static lv_obj_t* obj_container = NULL;
 
 bool isStatisticsPageActive = false;
 
@@ -19,45 +20,64 @@ void ui_statistics_init(void *data)
 {
     (void)data;
 
-    obj_fullstats = lv_table_create(lv_scr_act(), NULL);
-    lv_obj_set_size(obj_fullstats, 460, 250);
+    obj_container = lv_page_create(lv_scr_act(), NULL);
+    lv_obj_set_size(obj_container, 460, 250);
+    lv_obj_align(obj_container, NULL, LV_ALIGN_CENTER, 0, 20);
+    lv_page_set_scrl_layout(obj_container, LV_LAYOUT_PRETTY_TOP);
+    lv_page_set_scrl_height(obj_container, 250);
+    lv_page_set_scrlbar_mode(obj_container, LV_SCROLLBAR_MODE_ON);
+
+    obj_fullstats = lv_table_create(obj_container, NULL);
+    // lv_obj_set_size(obj_fullstats, 460, 250);
     lv_obj_set_style_local_bg_color(obj_fullstats, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
     lv_obj_set_style_local_bg_color(obj_fullstats, LV_OBJ_PART_MAIN, LV_STATE_PRESSED, LV_COLOR_WHITE);
     lv_obj_set_style_local_radius(obj_fullstats, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, 5);
     lv_obj_set_style_local_border_width(obj_fullstats, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, 0);
-    lv_obj_align(obj_fullstats, NULL, LV_ALIGN_CENTER, 0, 20);
-
     lv_table_set_col_cnt(obj_fullstats, 2);
     lv_table_set_row_cnt(obj_fullstats, 4);
 
-    lv_table_set_col_width(obj_fullstats, 0, 200);
-    lv_table_set_col_width(obj_fullstats, 1, 50);
-
+    lv_table_set_col_width(obj_fullstats, 0, 260);
+    lv_table_set_col_width(obj_fullstats, 1, 100);
 
     lv_table_set_cell_align(obj_fullstats, 0, 1, LV_LABEL_ALIGN_RIGHT);
     lv_table_set_cell_align(obj_fullstats, 1, 1, LV_LABEL_ALIGN_RIGHT);
     lv_table_set_cell_align(obj_fullstats, 2, 1, LV_LABEL_ALIGN_RIGHT);
     lv_table_set_cell_align(obj_fullstats, 3, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_align(obj_fullstats, 4, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_align(obj_fullstats, 5, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_align(obj_fullstats, 6, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_align(obj_fullstats, 7, 1, LV_LABEL_ALIGN_RIGHT);
 
     lv_table_set_cell_value(obj_fullstats, 0, 0, "Short Coffee");
     lv_table_set_cell_value(obj_fullstats, 1, 0, "Medium Coffee");
     lv_table_set_cell_value(obj_fullstats, 2, 0, "Long Coffee");
-    lv_table_set_cell_value(obj_fullstats, 3, 0, "Cappuccino");
+    lv_table_set_cell_value(obj_fullstats, 3, 0, "Free Coffee");
+    lv_table_set_cell_value(obj_fullstats, 4, 0, "Short Cappuccino");
+    lv_table_set_cell_value(obj_fullstats, 5, 0, "Medium Cappuccino");
+    lv_table_set_cell_value(obj_fullstats, 6, 0, "Double Cappuccino");
+    lv_table_set_cell_value(obj_fullstats, 7, 0, "Hot Milk");
 
     lv_table_set_cell_value(obj_fullstats, 0, 1, "24");
     lv_table_set_cell_value(obj_fullstats, 1, 1, "13");
     lv_table_set_cell_value(obj_fullstats, 2, 1, "2");
     lv_table_set_cell_value(obj_fullstats, 3, 1, "6");
+    lv_table_set_cell_value(obj_fullstats, 4, 1, "34");
+    lv_table_set_cell_value(obj_fullstats, 5, 1, "12");
+    lv_table_set_cell_value(obj_fullstats, 6, 1, "9");
+    lv_table_set_cell_value(obj_fullstats, 7, 1, "0");
+
+    // lv_obj_set_height(obj_fullstats, 250);
 }
 
 void ui_statistics_show(void *data)
 {
-    if(NULL == obj_fullstats)
+    if(NULL == obj_container)
     {
         ui_statistics_init(data);
     }
     else
     {
+        lv_obj_set_hidden(obj_container, false);
         lv_obj_set_hidden(obj_fullstats, false);
     }
 
@@ -68,8 +88,9 @@ void ui_statistics_hide(void *data)
 {
     (void)data;
 
-    if(NULL != obj_fullstats)
+    if(NULL != obj_container)
     {
+        lv_obj_set_hidden(obj_container, true);
         lv_obj_set_hidden(obj_fullstats, true);
     }
 
