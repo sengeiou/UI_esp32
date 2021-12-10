e6cc5c3756529882344795e7b380158a23a93a85
Giuseppe Lafortezza <giuseppe.lafortezza@lavazza.com>
2021-12-10 08:57:28 +0100

    Added fault icon

--- firmware/main/src/gui/ui_fault.cpp
+++ firmware/main/src/gui/ui_fault.cpp
@@ -17,21 +17,29 @@ ui_func_desc_t ui_fault_func = {
 
 // /* LVGL objects defination */
 static lv_obj_t* obj_label = NULL;
+static lv_obj_t* obj_image = NULL;
 
-/* Static function forward declaration */
+extern void* data_fault;
+
+
 
 void ui_fault_init(void *data)
 {
     (void)data;
 
+    obj_image = lv_img_create(lv_scr_act(), NULL);
+    lv_img_set_src(obj_image, data_fault);
+    lv_img_set_zoom(obj_image, 256);
+    lv_obj_align(obj_image, NULL, LV_ALIGN_CENTER, 0, -30);
+
     obj_label = lv_label_create(lv_scr_act(), NULL);
     lv_obj_set_style_local_bg_color(obj_label, LV_BAR_PART_BG, LV_STATE_DEFAULT, LV_COLOR_BLACK);
     lv_obj_set_style_local_text_color(obj_label, LV_BAR_PART_BG, LV_STATE_DEFAULT, LV_COLOR_RED);
-    lv_obj_set_size(obj_label, 480, 320);
+    lv_obj_set_size(obj_label, 400, 60);
     lv_obj_set_style_local_value_font(obj_label, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &font_en_bold_48);
-    lv_obj_align(obj_label, NULL, LV_ALIGN_CENTER, 0, 0);
-    lv_label_set_align(obj_label, LV_LABEL_ALIGN_CENTER);
+    lv_obj_align(obj_label, obj_image, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
     lv_label_set_text(obj_label, "FAULT");
+    lv_label_set_align(obj_label, LV_LABEL_ALIGN_CENTER);
     lv_obj_set_auto_realign(obj_label, true);
 }
 
@@ -44,6 +52,7 @@ void ui_fault_show(void *data)
     }
     else
     {
+        lv_obj_set_hidden(obj_image, false);
         lv_obj_set_hidden(obj_label, false);
     }
 }
@@ -54,6 +63,7 @@ void ui_fault_hide(void *data)
     
     if(NULL != obj_label)
     {
+        lv_obj_set_hidden(obj_image, true);
         lv_obj_set_hidden(obj_label, true);
     }
 }
