e0c6f4446e9eb57dbb63ab765108b45bd5b8833f
Giuseppe Lafortezza <giuseppe.lafortezza@lavazza.com>
2022-01-12 17:58:47 +0100

    Increased wifi stack

--- firmware/main/src/main.cpp
+++ firmware/main/src/main.cpp
@@ -185,7 +185,7 @@ void start_tasks()
     utils::system::start_thread_core(&gui_task, &xHandleGui, "gui_task", 1024*16, 4, 1);
 
     if(true == machineConnectivity.wifiEnabled)
-        utils::system::start_thread_core(&wifi_task, &xHandleWiFi, "wifi_task", 1024*6, 4, 1);
+        utils::system::start_thread_core(&wifi_task, &xHandleWiFi, "wifi_task", 1024*8, 4, 0);
 }
 
 void main_task(void* p)
