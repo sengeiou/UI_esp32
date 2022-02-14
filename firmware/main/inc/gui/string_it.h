272c283f9dad4872f372c2cea0cb1d9df27f4dce
Giuseppe Lafortezza <giuseppe.lafortezza@lavazza.com>
2022-02-14 18:09:08 +0100

    Added italian language support

---
+++ firmware/main/inc/gui/string_it.h
@@ -0,0 +1,26 @@
+#ifndef _STRINGS_IT_H_
+#define _STRINGS_IT_H_
+
+#define STANDBY_MESSAGE         "Premi o scansiona il QR code"
+
+#define PREP_LABEL_1            "ESPRESSO CORTO"
+#define PREP_LABEL_2            "ESPRESSO"
+#define PREP_LABEL_3            "ESPRESSO LUNGO"
+#define PREP_LABEL_4            "MACCHIATO"
+#define PREP_LABEL_5            "CAPPUCCINO"
+#define PREP_LABEL_6            "LATTE MACCHIATO"
+#define PREP_LABEL_7            "DOSE LIBERA"
+#define PREP_LABEL_8            "CAFFE AMERICANO"
+#define PREP_LABEL_9            "ACQUA CALDA"
+
+#define EROG_DRINK_READY        "Bevanda pronta.\nRitirare"
+
+#define WARNBAR_POD_FULL_MESSAGE                "Cassetto capsule pieno"
+#define WARNBAR_POD_REMOVED_MESSAGE             "Cassetto capsule rimosso"
+#define WARNBAR_POD_GENERIC_MESSAGE             "Cassetto capsule pieno o rimosso"
+#define WARNBAR_WATER_EMPTY_MESSAGE             "Rilevata mancanza acqua"
+#define WARNBAR_DESCALING_NEEDED_MESSAGE        "Necessaria procedura di decalcifica"
+#define WARNBAR_DESCALING_INPROGRESS_MESSAGE    "Procedura di decalcifica in corso. Attendere il completamento..."
+#define WARNBAR_GENERIC_WARNING_MESSAGE         "Errore generico. Contattare l'assistenza"
+
+#endif /* _STRINGS_IT_H_ */
